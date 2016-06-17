0149d936fceaafdd6aa26b32223db045415cb631
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-06-17 21:30:57 +0200

    Added projects page.

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,19 +1,23 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-    .module('app.portfolio')
-    .config(routerConfig);
+    angular
+        .module('app.portfolio')
+        .config(routerConfig);
 
-  /** @ngInject */
-  function routerConfig($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/pages/home/home.html'
-      });
+    /** @ngInject */
+    function routerConfig($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'app/pages/home/home.html'
+            })
+            .state('projects', {
+                url: '/projecten',
+                templateUrl: 'app/pages/projects/projects.html'
+            })
 
-    $urlRouterProvider.otherwise('/');
-  }
+        $urlRouterProvider.otherwise('/');
+    }
 
 })();
