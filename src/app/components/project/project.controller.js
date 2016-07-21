b2b40c963acad28ab2a3f72e5fcd2dfc511d1c1d
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-07-21 20:53:15 +0200

    Added project controller.

---
+++ src/app/components/project/project.controller.js
@@ -0,0 +1,13 @@
+/**
+ * Created on 21/07/16.
+ * @author Jurgen Romeijn
+ */
+(function (angular) {
+    'use strict';
+
+    function ProjectController($stateParams) {
+        
+    }
+
+    angular.module('app.portfolio').controller('projectController', ProjectController);
+})(window.angular);
