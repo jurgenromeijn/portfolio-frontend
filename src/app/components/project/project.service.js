6ab6b115131b9b9a62dd50ae1fc12b842cddec57
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-07-21 21:10:14 +0200

    Added method to projectService to find a single project by it's slug.

--- src/app/components/project/project.service.js
+++ src/app/components/project/project.service.js
@@ -5,25 +5,38 @@
 (function (angular) {
     'use strict';
 
-    function ProjectService($http, $q) {
+    function ProjectService($http, $q, lodash) {
         this._$http = $http;
         this._$q = $q;
+        this._lodash = lodash;
 
         this._projects = null;
     }
 
     ProjectService.prototype.getProjects = function () {
         var that = this,
-            projectDefer = this._$q.defer();
+            projectsDefer = this._$q.defer();
 
         if (this._projects === null) {
             this._$http.get('http://localhost:8000/wp-json/projects/v1/project').then(function (response) {
                 that._projects = response.data;
-                projectDefer.resolve(response.data);
+                projectsDefer.resolve(response.data);
             });
         } else {
-            projectDefer.resolve(this._projects);
+            projectsDefer.resolve(this._projects);
         }
+
+        return projectsDefer.promise;
+    };
+
+    ProjectService.prototype.getProject = function (slug) {
+        var that = this,
+            projectDefer = this._$q.defer();
+
+        this.getProjects().then(function (projects) {
+            var project = that._lodash.find(projects, {'slug': slug})[0];
+            projectDefer.resolve(project);
+        });
 
         return projectDefer.promise;
     };
