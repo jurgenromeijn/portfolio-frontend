430cd4fdbbf56838324ae90414a24a0c99905c74
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-07-21 21:16:06 +0200

    find project by slug in controller.

--- src/app/components/project/project.controller.js
+++ src/app/components/project/project.controller.js
@@ -5,9 +5,20 @@
 (function (angular) {
     'use strict';
 
-    function ProjectController($stateParams) {
-        
+    function ProjectController($stateParams, projectService) {
+        this._projectService = projectService;
+
+        this.project = null;
+
+        this.findProject($stateParams.slug);
     }
+
+    ProjectController.prototype.findProject = function (slug) {
+        var that = this;
+        this._projectService.findProject(slug).then(function (project) {
+            that.project = project;
+        });
+    };
 
     angular.module('app.portfolio').controller('projectController', ProjectController);
 })(window.angular);
