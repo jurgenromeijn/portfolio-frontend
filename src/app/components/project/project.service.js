9509cfa44ee2fb09e29b49d8ada7a64488770fe1
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-07-21 08:36:30 +0200

    Created the ProjectService.

---
+++ src/app/components/project/project.service.js
@@ -0,0 +1,13 @@
+/**
+ * Created on 21/07/16.
+ * @author Jurgen Romeijn
+ */
+(function (angular) {
+    'use strict';
+
+    function ProjectService() {
+
+    }
+
+    angular.module('app.portfolio').service('ProjectService', ProjectService);
+})(window.angular);
