622d2696cb26054894cbdcb6330f39b2308c86b0
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-07-23 15:20:43 +0200

    assigned all properties to the view model variable.

--- src/app/components/project/project.controller.js
+++ src/app/components/project/project.controller.js
@@ -2,26 +2,28 @@
  * Created on 21/07/16.
  * @author Jurgen Romeijn
  */
-(function (angular) {
+(function () {
     'use strict';
 
     function ProjectController($stateParams, $sce, projectService) {
-        this._projectService = projectService;
-        this._$sce = $sce;
+        var vm = this;
 
-        this.title = "";
-        this.content = "";
+        vm._projectService = projectService;
+        vm._$sce = $sce;
 
-        this.getProject($stateParams.slug);
+        vm.title = "";
+        vm.content = "";
+
+        vm.getProject($stateParams.slug);
     }
 
     ProjectController.prototype.getProject = function (slug) {
-        var that = this;
-        this._projectService.getProject(slug).then(function (project) {
-            that.title = project.title;
-            that.content = that._$sce.trustAsHtml(project.content);
+        var vm = this;
+        vm._projectService.getProject(slug).then(function (project) {
+            vm.title = project.title;
+            vm.content = vm._$sce.trustAsHtml(project.content);
         });
     };
 
     angular.module('app.portfolio').controller('projectController', ProjectController);
-})(window.angular);
+})();
