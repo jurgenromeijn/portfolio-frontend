fbac3e49661fffc7674110ef30eaa5bbb431fef6
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-07-23 15:52:08 +0200

    Simplified the ProjectService.

--- src/app/components/project/project.service.js
+++ src/app/components/project/project.service.js
@@ -2,49 +2,43 @@
  * Created on 21/07/16.
  * @author Jurgen Romeijn
  */
-(function (angular) {
+(function () {
     'use strict';
 
     function ProjectService($http, $q, lodash, api) {
-        this._$http = $http;
-        this._$q = $q;
-        this._lodash = lodash;
-        this._api = api;
-
         this._projects = null;
+
+        this.getProjects = function () {
+            var that = this,
+                projectsDefer = $q.defer();
+
+            if (this._projects === null) {
+                $http.get(getProjectApiUrl()).then(function (response) {
+                    that._projects = response.data;
+                    projectsDefer.resolve(response.data);
+                });
+            } else {
+                projectsDefer.resolve(this._projects);
+            }
+
+            return projectsDefer.promise;
+        };
+
+        this.getProject = function (slug) {
+            var projectDefer = $q.defer();
+
+            this.getProjects().then(function (projects) {
+                var project = lodash.find(projects, {'slug': slug});
+                projectDefer.resolve(project);
+            });
+
+            return projectDefer.promise;
+        };
+
+        function getProjectApiUrl() {
+            return [api.baseUrl, api.endpoints.projects].join('');
+        }
     }
 
-    ProjectService.prototype.getProjects = function () {
-        var that = this,
-            projectsDefer = this._$q.defer();
-
-        if (this._projects === null) {
-            this._$http.get(this._getProjectApiUrl()).then(function (response) {
-                that._projects = response.data;
-                projectsDefer.resolve(response.data);
-            });
-        } else {
-            projectsDefer.resolve(this._projects);
-        }
-
-        return projectsDefer.promise;
-    };
-
-    ProjectService.prototype.getProject = function (slug) {
-        var that = this,
-            projectDefer = this._$q.defer();
-
-        this.getProjects().then(function (projects) {
-            var project = that._lodash.find(projects, {'slug': slug});
-            projectDefer.resolve(project);
-        });
-
-        return projectDefer.promise;
-    };
-
-    ProjectService.prototype._getProjectApiUrl = function () {
-        return [this._api.baseUrl, this._api.endpoints.projects].join('');
-    };
-
     angular.module('app.portfolio').service('projectService', ProjectService);
-})(window.angular);
+})();
