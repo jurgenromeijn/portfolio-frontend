54dbd0341d4c0fd3cdf6257163fbc8ba694db312
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-08-10 21:28:41 +0200

    Added contact form endpoint to constants.

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -4,9 +4,10 @@
 
   angular.module('app.portfolio')
       .constant('api', {
-        'baseUrl': 'http://localhost:8000',
-        'endpoints': {
-          'projects': '/wp-json/projects/v1/project'
-        }
+          'baseUrl': 'http://localhost:8000',
+          'endpoints': {
+              'projects': '/wp-json/projects/v1/project',
+              'contact': '/?v=1470851965'
+          }
       });
 })();
