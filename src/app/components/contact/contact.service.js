e2b12b29be65629f3b97476d4fbf3c0c3b8a02bd
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-08-11 21:47:32 +0200

    implemented methods to get the form definition.

--- src/app/components/contact/contact.service.js
+++ src/app/components/contact/contact.service.js
@@ -6,27 +6,56 @@
     'use strict';
 
     /**
-     * A service used for sending contact data.
+     * A service used for sending a contact form.
      * @param $http
      * @param api
      * @constructor
      */
-    function ContactService($http, api) {
+    function ContactService($http, $q, api) {
+        var formDefinitions = [];
+
+        /**
+         * Get the definition of a contact form.
+         * @param id
+         * @returns {Promise}
+         */
+        this.getFormDefinition = function (id) {
+            return $q(function (resolve) {
+                if (angular.isUndefined(formDefinitions[id])) {
+                    $http.get(getFormDefinitionApiUrl(id)).then(function (response) {
+                        formDefinitions[id] = response.data;
+                        resolve(response.data);
+                    });
+                } else {
+                    resolve(formDefinitions[id]);
+                }
+            });
+        };
+
         /**
          * Submit the contact data.
          * @param data
          * @returns {Promise}
          */
         this.submit = function (data) {
-            return $http.post(geContactApiUrl(), data);
+            return $http.post(geContactSubmitApiUrl(), data);
         };
 
         /**
-         * Get the url of the contact api.
+         * Get the url of the form definition api.
+         * @param id
          * @returns {string}
          */
-        function geContactApiUrl() {
-            return [api.baseUrl, api.endpoints.contact].join('');
+        function getFormDefinitionApiUrl(id) {
+            return [api.baseUrl, api.endpoints.contact.formDefinitions, id].join('/');
+        }
+
+        /**
+         * Get the url of the contact submit api.
+         * @returns {string}
+         */
+        function geContactSubmitApiUrl() {
+            return [api.baseUrl, api.endpoints.contact.submit].join('/');
         }
     }
 
