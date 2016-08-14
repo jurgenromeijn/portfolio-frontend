3f7cf591123183ae75f323d1cf576cb9f4d4846b
Jurgen Romeijn <jurgen.romeijn@incentro.com>
2016-08-14 14:34:55 +0200

    updated projects endpoint.

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -6,7 +6,7 @@
       .constant('api', {
           baseUrl: 'http://localhost:8000',
           endpoints: {
-              projects: 'wp-json/projects/v1/project',
+              projects: 'wp-json/projects/v1/projects',
               contact: {
                   formDefinitions: 'wp-json/ccf/v1/forms',
                   submit: '?v=1470851965'
